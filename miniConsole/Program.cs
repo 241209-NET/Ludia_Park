33f37d6c878743db1f19ad99a01413787333e111
Ludia Park <lypark1985@gmail.com>
2024-12-10 20:28:17 -0600

    edit

--- miniConsole/Program.cs
+++ miniConsole/Program.cs
@@ -20,6 +20,6 @@ Transform or do some calculation with that data
 
 - validate input: maybe the user inputs their age or something, and maybe you gotta make sure it is a number and not some letters.
 - use a collection: maybe it's a list of something
-- have a menu with 3 options: this is just a switch statement, such as add a movie, add new users, add a new car, etc.  one option can even be to quit the app. (we never learned menu so maybe i can do this part next time?)
+- have a menu with 3 options: this is just a switch statement, such as add a movie, add new users, add a new car, etc.  one option can even be to quit the app. (maybe i can do this part next time?)
 - manipulate data: maybe u have a list of movies, and u can get the average rating of the movie.  maybe a user can input a movie name and a rating, and one of the "options"?  (maybe the menu?) you can get the calculated average rating.
 */
