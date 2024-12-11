dec92db53db62dc14fac6644698d3d8394897924
Ludia Park <lypark1985@gmail.com>
2024-12-10 20:27:35 -0600

    instructions

--- miniConsole/Program.cs
+++ miniConsole/Program.cs
@@ -7,3 +7,19 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+
+
+/*
+C# .NET Console APP
+Take user input
+Input validation
+Use a collection to store data
+Have a menu with atleast 3 options
+Transform or do some calculation with that data
+////////////////////
+
+- validate input: maybe the user inputs their age or something, and maybe you gotta make sure it is a number and not some letters.
+- use a collection: maybe it's a list of something
+- have a menu with 3 options: this is just a switch statement, such as add a movie, add new users, add a new car, etc.  one option can even be to quit the app. (we never learned menu so maybe i can do this part next time?)
+- manipulate data: maybe u have a list of movies, and u can get the average rating of the movie.  maybe a user can input a movie name and a rating, and one of the "options"?  (maybe the menu?) you can get the calculated average rating.
+*/
