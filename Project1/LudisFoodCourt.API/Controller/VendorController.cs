726604c24afc8dd9d116eaf0a7aee585e270de2e
Ludia Park <lypark1985@gmail.com>
2024-12-22 21:46:20 -0600

    did vendor model

--- Project1/LudisFoodCourt.API/Controller/VendorController.cs
+++ Project1/LudisFoodCourt.API/Controller/VendorController.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 
+namespace LudisFoodCourt.Api.Controller;
+
 [Route("/vendors")]
 [ApiController]   // auto validates model of incoming req's.
 public class VendorController : ControllerBase
 {
-  // [HttpGet]
-  // public IActionResult GetAllVendors()
-  // {
-    
-  // }
+  [HttpGet]
+  public IActionResult GetAllVendors()
+  {
+    return Ok("list of vendors put in later");
+  }
 }
---
+++ Project1/LudisFoodCourt.API/Model/Vendor.cs
@@ -0,0 +1,8 @@
+namespace LudisFoodCourt.Api.Model;
+
+public class Vendor 
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string FoodType { get; set; }
+}
