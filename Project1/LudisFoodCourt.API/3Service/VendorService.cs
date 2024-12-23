c58b220fde0f1cf97d35fdf3375cb48dd4413216
Ludia Park <lypark1985@gmail.com>
2024-12-22 23:47:30 -0600

    vendorService implements ivendorservice

--- Project1/LudisFoodCourt.API/3Service/VendorService.cs
+++ Project1/LudisFoodCourt.API/3Service/VendorService.cs
@@ -2,10 +2,25 @@ using LudisFoodCourt.Api.Model;
 
 namespace LudisFoodCourt.Api.Service;
 
-public class VendorService
+public class VendorService : IVendorService
 {
-  public List<Vendor> GetAllVendors()
+  public Food AddFoodToMenu(int vendorId, Food food)
   {
-    
+    throw new NotImplementedException();
+  }
+
+  public Vendor CreateVendor(Vendor vendor)
+  {
+    throw new NotImplementedException();
+  }
+
+  public IEnumerable<Food> GetAllFoodsOfVendor(int vendorId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public IEnumerable<Vendor> GetAllVendors()
+  {
+    throw new NotImplementedException();
   }
 }
