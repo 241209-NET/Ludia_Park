3aec17773571822286fed984341b7e0121d572db
Ludia Park <lypark1985@gmail.com>
2024-12-22 21:52:46 -0600

    finished model validations for vendor

--- Project1/LudisFoodCourt.API/Model/Vendor.cs
+++ Project1/LudisFoodCourt.API/Model/Vendor.cs
@@ -1,8 +1,16 @@
 namespace LudisFoodCourt.Api.Model;
+using System.ComponentModel.DataAnnotations;
 
 public class Vendor 
 {
+  [Key]
   public int Id { get; set; }
+  
+  [Required]
+  [MaxLength(100)]
   public string Name { get; set; }
+
+  [Required]
+  [MaxLength(100)]
   public string FoodType { get; set; }
 }
