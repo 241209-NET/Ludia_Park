1aebdc3bfd0417cee78106846b7b643dabf05088
Ludia Park <lypark1985@gmail.com>
2024-12-24 17:24:10 -0600

    most of vendor controller, need to implement getVendorById everywhere

--- Project1/LudisFoodCourt.API/2Controller/VendorsController.cs
+++ Project1/LudisFoodCourt.API/2Controller/VendorsController.cs
@@ -15,33 +15,35 @@ public class VendorsController : ControllerBase
   }
 
   [HttpGet("{vendorId}/foods")]
-  public IActionResult GetFoodsOfVendor(int vendorId)
+  public IActionResult GetAllFoodsOfVendor(int vendorId)
   {
     var foodsOfVendor = _vendorService.GetAllFoodsOfVendor(vendorId);
     return Ok(foodsOfVendor);
   }
 
-  // [HttpPost("{vendorId}/foods")]
-  // public IActionResult AddFoodToMenu(int vendorId, Food food)
-  // {
-  //   var newFood = 
-  //   return Created(newFood);
-  // }
+  [HttpPost("{vendorId}/foods")]
+  public IActionResult AddFoodToMenu(int vendorId, Food food)
+  {
+    var newFood = _vendorService.AddFoodToMenu(vendorId, food);
+
+    return CreatedAtAction(nameof(GetFoodById), new { id = newFood.Id }, newFood);
+  }
+
+  [HttpGet("{vendorId}")]
 
   [HttpGet]
   public IActionResult GetAllVendors()
   {
-    return Ok("list of vendors put in later");
-    // var vendors = _vendorService.GetAllVendors();
-    // return Ok(vendors);
+    var vendors = _vendorService.GetAllVendors();
+    return Ok(vendors);
   }
 
-  // [HttpPost]
-  // public IActionResult CreateVendor(Vendor vendor)
-  // {
-  //   var newVendor = 
-  //   return Created(newVendor);
-  // }
+  [HttpPost]
+  public IActionResult CreateVendor(Vendor vendor)
+  {
+    var newVendor = _vendorService.CreateVendor(vendor);
+    return CreatedAtAction(nameof(GetVendorById), new { id = newVendor.Id }, newVendor);
+  }
 }
 
 
@@ -51,4 +53,10 @@ public class VendorsController : ControllerBase
 - Private readonly: The _vendorService field will hold the instance of IVendorService 
   that the controller will use to interact with your business logic layer. 
   It’s marked as readonly, which means it can only be assigned once (in the constructor).
+- in order to achieve Created 201 status, use CreatedAtAction w/ these params:
+  1) a method to retrieve this new record
+  2) route param
+  3) new object
+- GetFoodById endpoint must be made in order for this Created status to work.
+  (even though I don't need it for my app)
 */
