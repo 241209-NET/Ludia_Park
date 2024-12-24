ffde50579b2304309b00a0854ceeefd24bd314c6
Ludia Park <lypark1985@gmail.com>
2024-12-24 17:48:04 -0600

    i think i fixed Foods table, but can't build till make all layers

--- Project1/LudisFoodCourt.API/1Model/Vendor.cs
+++ Project1/LudisFoodCourt.API/1Model/Vendor.cs
@@ -1,18 +1,14 @@
 namespace LudisFoodCourt.Api.Model;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
 
 public class Vendor 
 {
-  [Key]
-  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
   public int Id { get; set; }
   
-  [Required]
   [MaxLength(100)]
   public string Name { get; set; }
 
-  [Required]
   [MaxLength(100)]
   public string FoodType { get; set; }
 
