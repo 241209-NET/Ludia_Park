cf3ed799624d6fc2ed781c34653b407b6b44339b
Ludia Park <lypark1985@gmail.com>
2024-12-30 14:01:55 -0600

    included javascript

---
+++ HTMLEx/index.js
@@ -0,0 +1,8 @@
+function addGlow(imageId) {
+  const image = document.getElementById(imageId);
+  // Toggle the glow effect by adding/removing the 'glow' class
+  image.classList.toggle('glow');
+}
+
+
+
